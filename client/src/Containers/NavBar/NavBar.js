eef0fff1d10f361dea45a9b94dcebfb439e7e980
Jason Hughes <jasonhughes088@gmail.com>
2018-03-27 18:56:41 -0600

    update some basic styles

--- client/src/Containers/NavBar/NavBar.js
+++ client/src/Containers/NavBar/NavBar.js
@@ -14,7 +14,7 @@ export class NavBar extends Component {
     return (
       <div className='nav-bar'>
         <img className="logo" src={require('../../assets/turing.png')} alt="logo" />
-        <h1 className='main-header'>Turing Salaries</h1>
+        <h1 className='main-header'>Movement Academy</h1>
       </div>
     );
   }
